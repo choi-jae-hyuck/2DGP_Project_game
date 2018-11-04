6a468afe8d0b2f0da75e0566aaaf0e8239d74b52
choi-jae-hyuck <cgear@naver.com>
2018-11-04 20:49:19 +0900

    fish 애니메이션
    idle running attack 애니메이션만 만듬

--- fish.py
+++ fish.py
@@ -5,18 +5,20 @@ import game_world
 
 import main_state
 
-class FISH:
+class Fish:
     def __init__(self):
         self.x, self.y= 300,100
-        self.state=0
+        self.state=1
         self.frame=0
         self.image=load_image('enemy1.png')
 
     def draw(self):
         if(self.state==0):# idle
-            self.image.clip_draw(self.frame * 34 +2,1485 * 1, 31, 31, self.x,100+ self.y)
-        elif(self.state==1): #running
-            self.image.clip_draw(self.frame * 34 +2,1451 * 1, 31, 31, self.x,100+ self.y)
+            self.image.clip_draw(self.frame * 31+2 ,743, 31, 31, self.x,100+ self.y,50,50)
+        if(self.state==1):# running
+            self.image.clip_draw(self.frame * 35 ,668, 36, 38, self.x,100+ self.y,50,50)
+        elif(self.state==2): #attack
+            self.image.clip_draw(self.frame * 29+1,708 * 1, 29, 31, self.x,100+ self.y,50,50)
 
     def update(self):
         if(self.state==0):
@@ -24,7 +26,6 @@ class FISH:
                 self.frame=0
             self.frame=(self.frame+1)%4
         elif(self.state==1):
-            self.frame=(self.frame+1)%8
-        elif(self.state==3):
-            if(self.frame>11-1):
-                self.frame=0
+            self.frame=(self.frame+1)%3
+        elif(self.state==2):
+            self.frame=(self.frame+1)%5
