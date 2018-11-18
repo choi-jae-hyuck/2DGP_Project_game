2ddc996237395b167afa549b7ee5325c9122356e
choi-jae-hyuck <cgear@naver.com>
2018-11-18 19:11:19 +0900

    composite로 변경
    fish

--- fish.py
+++ fish.py
@@ -12,7 +12,7 @@ FRAMES_PER_ACTION = 6
 class Fish:
     def __init__(self):
         self.x, self.y= 300,100
-        self.state=1
+        self.state=0
         self.frame=0
         self.timer=0
         self.image=load_image('Resource\character\enemy1.png')
@@ -20,16 +20,17 @@ class Fish:
 
     def draw(self):
         if(self.state==0):# idle
-            self.image.clip_draw(int(self.frame) * 31+2 ,743, 31, 31, self.x,100+ self.y,50,50)
+            self.image.clip_composite_draw(int(self.frame) * 31 + 2, 743, 31, 31, -3.141592, 'v', self.x, 100 + self.y,50, 50)
         if(self.state==1):# running
-            self.image.clip_draw(int(self.frame) * 35 ,668, 36, 38, self.x,100+ self.y,50,50)
+            self.image.clip_composite_draw(int(self.frame) * 35 ,668, 36, 38, self.x,100+ self.y,50,50)
         elif(self.state==2): #attack
-            self.image.clip_draw(int(self.frame) * 29+1,708 * 1, 29, 31, self.x,100+ self.y,50,50)
+            self.image.clip_composite_draw(int(self.frame) * 29+1,708 * 1, 29, 31, self.x,100+ self.y,50,50)
 
 
     def update(self):
 
         self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 5
+
         if(self.state==0):
             if(self.frame>4-1):
                 self.frame=0
@@ -38,6 +39,9 @@ class Fish:
             self.frame=(self.frame+1)%3
         elif(self.state==2):
             self.frame=(self.frame+1)%5
+
+        if game_framework.turn==False:
+            pass
 
 
     def handle_event(self, event):
