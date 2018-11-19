70f45fe53e8ba1628e8e48bbb0d2167f93fe9ccd
choi-jae-hyuck <cgear@naver.com>
2018-11-19 23:58:49 +0900

    refactoring
    is 로 바꾸기

--- fish.py
+++ fish.py
@@ -21,52 +21,52 @@ class Fish:
 
     def draw(self):
         if(self.dir==True):
-            if(self.state==0):# idle
+            if self.state is 0:# idle
                 self.image.clip_composite_draw(int(self.frame) * 31 + 2, 743, 31, 31, -3.141592, 'v', self.x, 100 + self.y,50, 50)
-            if(self.state==1):# running
+            if self.state is 1:# running
                 self.image.clip_composite_draw(int(self.frame) * 35 ,668, 36, 38, -3.141592, 'v', self.x,100+ self.y,50,50)
-            elif(self.state==2): #attack
+            elif self.state is 2: #attack
                 self.image.clip_composite_draw(int(self.frame) * 29+1,708 * 1, 29, 31, -3.141592, 'v', self.x,100+ self.y,50,50)
         elif (self.dir == False):
-            if (self.state == 0):  # idle
+            if self.state is 0:  # idle
                 self.image.clip_composite_draw(int(self.frame) * 31 + 2, 743, 31, 31, 180 * -3.141592, ' ', self.x,100 + self.y, 50, 50)
-            if (self.state == 1):  # running
+            if self.state is 1:  # running
                 self.image.clip_composite_draw(int(self.frame) * 35, 668, 36, 38, 180 * -3.141592, ' ', self.x, 100 + self.y, 50, 50)
-            elif (self.state == 2):  # attack
+            elif self.state is 2:  # attack
                 self.image.clip_composite_draw(int(self.frame) * 29 + 1, 708 * 1, 29, 31, 180 * -3.141592, ' ', self.x,100 + self.y, 50, 50)
 
 
     def update(self):
 
         self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 5
-        if(self.state==0):
+        if self.state is 0:
             self.frame=(self.frame)%4
-        elif(self.state==1):
+        elif self.state is 1:
             self.frame=(self.frame)%3
-        elif(self.state==2):
+        elif self.state is 2:
             self.frame=(self.frame)%5
 
-        if game_framework.turn==False:
+        if game_framework.turn is False:
             if main_state.hero.x-50<self.x <main_state.hero.x+50:
                 if main_state.hero.y-50<self.y <main_state.hero.y+50:
                     self.attack=True
             else :
                 self.attack=False
 
-            if self.attack==True:
+            if self.attack is True:
                 pass
-            elif self.attack==False:
-                if self.movement==1:
+            elif self.attack is False:
+                if self.movement is 1:
                     self.y+=5
-                elif self.movement==2:
+                elif self.movement is 2:
                     self.y-=5
-                elif self.movement==3:
+                elif self.movement is 3:
                     self.x-=5
-                elif self.movement==4:
+                elif self.movement is 4:
                     self.x+=5
                 print(self.x, self.y)
 
-        elif game_framework.turn==True:
+        elif game_framework.turn is True:
             self.movement = random.randint(1, 4)  # 상하좌우
 
     def handle_event(self, event):
